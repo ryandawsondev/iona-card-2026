106b241071ad23e2cedd714b19220d1cbcccbab0
ryandawsondev <ryan.dawson.dev@outlook.com>
2026-06-14 14:21:32 +0100

    updated for gh pages

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,7 @@ import { routeTree } from './routeTree.gen'
 export function getRouter() {
   const router = createTanStackRouter({
     routeTree,
+    basepath: '/iona-card-2026',
     scrollRestoration: true,
     defaultPreload: 'intent',
     defaultPreloadStaleTime: 0,
